eaa9cc23d803a14515519e69011c56b385cee7cf
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-04-01 19:44:17 +0530

    local change

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 console.info("This is an info message-V1");//for informational messages
 console.warn("This is a warning message-V1");//for warning messages
 console.error("This is an error message-V1");//for error messages
+console.log("Change from Local");
