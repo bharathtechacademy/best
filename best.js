35de47d61bdb6d87e443b49032e4131daf96a78c
bharathtechacademy <139088966+bharathtechacademy@users.noreply.github.com>
2026-04-01 19:58:42 +0530

    Update best.js

--- best.js
+++ best.js
@@ -1,3 +1,4 @@
 console.info("This is an info message-V1");
 console.warn("This is a warning message-V1");
 console.error("This is an error message-V1");
+console.error("This is an error message-V2");
