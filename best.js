427576b1f2e3412418a0c9988c75c916f84c0948
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-04-01 19:39:31 +0530

    test

---
+++ best.js
@@ -0,0 +1,3 @@
+console.info("This is an info message-V1");
+console.warn("This is a warning message-V1");
+console.error("This is an error message-V1");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.info("This is an info message-V1");//for informational messages
+console.warn("This is a warning message-V1");//for warning messages
+console.error("This is an error message-V1");//for error messages
